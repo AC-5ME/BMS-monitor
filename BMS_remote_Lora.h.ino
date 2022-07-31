96c9862e26415bf67c983cf924c40fa824297cbe
AC-5ME <99979525+AC-5ME@users.noreply.github.com>
2022-07-31 17:33:28 -0600

    Working TX

--- BMS_remote_Lora.h.ino
+++ BMS_remote_Lora.h.ino
@@ -13,15 +13,15 @@ Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
 #define PIEZO_PIN 5 // Piezo speaker/buzzer pin
 
 void setup() {
-  Serial.begin(9600);
   LoRa.setPins(8, 4, 7);      //CS, RST, INT
+  //LoRa.setSyncWord(0xBB);
 
   delay(250); // wait for the OLED to power up
   display.begin(0x3C, true); // Address 0x3C default
   display.clearDisplay();
   display.display();
   display.setRotation(1);
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
   display.setTextColor(1, 0);
   display.setCursor(0, 0);
@@ -46,34 +46,29 @@ void loop() {
   measuredVbat /= 1024; // convert to voltage
   int batPercent = ((measuredVbat - batMin) / (span) * 100);
 
-  Serial.println(measuredVbat);
-
   if (batPercent > 100) {
     display.setCursor(0, 48);     //(Row, Column)
-    display.print("Charging?");
+    display.print("Remote charging?       ");
     display.display();
 
   } else {
     display.setCursor(0, 48);     //(Row, Column)
-    display.print("Batt: ");
+    display.print("Remote: ");
     display.print(batPercent);
-    display.print("%          ");
+    display.print("%  ");
+    display.print(measuredVbat);
+    display.print(" V ");
     display.display();
   }
 
   int packetSize = LoRa.parsePacket();
   if (packetSize) {     // received a packet?
 
-    Serial.println("Received packet: ");
     display.setCursor(0, 0);
 
     while (LoRa.available()) {
       display.print((char)LoRa.read());
-      display.print("   ");
-      display.print(" V");
       display.display();
-     }
-      display.print("      ");
-      display.display();
+    }
   }
 }

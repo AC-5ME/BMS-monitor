2788ebce419a53a88daa17c9a09ab1db9b7a4475
AC-5ME <99979525+AC-5ME@users.noreply.github.com>
2022-08-24 11:00:06 -0600

    Smoke alarm

--- BMS_remote.ino
+++ BMS_remote.ino
@@ -1,109 +1,113 @@
-
+#include <LoRa.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <RH_RF95.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
 Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
 
-#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 7
-
-// 32u4, M0, M4, nrf52840, esp32-s2 and 328p
 #define BUTTON_A  9
 #define BUTTON_B  6
 #define BUTTON_C  5
+#define VBATPIN A9
+#define PIEZO_PIN 5 // Piezo speaker/buzzer pin
 
-// Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 915.0
-
-// Singleton instance of the radio driver
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
-
-enum FSM_STATES {     //Recevied loop states
-  FSM_PRINT_PACK,
-  FSM_PRINT_MEAN,
-  FSM_PRINT_DEV,
-  FSM_PRINT_ALERTS,
+enum SERIAL_REQUEST {     //Sent msg states
+  P,      //Pack voltage
+  S,      //Smoke alarm
 };
 
-void setup() {
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-  pinMode(5, OUTPUT);      //Alarm buzzer
+void BatSignal_Display() {
+  int rssi = LoRa.packetRssi();     //Signal strength
+  float snr = LoRa.packetSnr();     //Signal/noise ratio
 
-  delay(250); // wait for the OLED to power up
-  display.begin(0x3C, true); // Address 0x3C default
+  const int batMin = 3.2;    //Remote battery % conversion
+  const int batMax = 4.3;
+  const int span = batMax - batMin;
+  float measuredVbat = analogRead(VBATPIN);
+  measuredVbat *= 2;    // we divided by 2, so multiply back
+  measuredVbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredVbat /= 1024; // convert to voltage
+  int batPercent = ((measuredVbat - batMin) / (span) * 100);
+
+  display.setCursor(0, 40);
+  display.print("Signal: ");
+  display.print(rssi);
+  display.print("/");
+  display.print(snr);
+  //display.print(" dBm ");
   display.display();
-  delay(1000);
+
+  /* if (batPercent > 100) {
+     display.setCursor(0, 48);     //(Row, Column)
+     display.print("Remote charging?       ");
+     display.display();
+
+    } else { */
+
+  display.setCursor(0, 48);
+  display.print("Remote: ");
+  display.print(batPercent);
+  display.print("%  ");
+  display.print(measuredVbat);
+  display.print("V ");
+  display.display();
+}
+
+void smoke() {
+  for (int i = 0; i < 5; i++) {
+    display.clearDisplay();
+    display.display();
+    display.setTextSize(3);
+    display.setCursor(15, 10);
+    display.print("Smoke!");
+    display.display();
+    //tone(5, 4000, 500);      //Pin, Freq, Duration
+    delay(500);
+  }
   display.clearDisplay();
   display.display();
+}
 
+void setup() {
+  LoRa.setPins(8, 4, 7);      //CS, RST, INT
+  //LoRa.setSyncWord(0xBB);     //Not working
+
+  display.begin(0x3C, true); // Address 0x3C default
+  display.clearDisplay();
+  display.display();
   display.setRotation(1);
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
   display.setTextColor(1, 0);
   display.setCursor(0, 0);
-  display.print("Pack: ");
-  display.setCursor(0, 10);
-  display.print("Mean Cell: ");
-  display.setCursor(0, 20);
-  display.print("Cell std dev: ");
-  display.setCursor(0, 30);
-  display.print("Alerts: ---");
-  display.display();
 
-  digitalWrite(RFM95_RST, LOW);     //Manual radio reset
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-
-  while (!rf95.init()) {
-    display.setCursor(48, 30);
-    display.print("Radio fail!");
+  if (!LoRa.begin(915E6)) {
+    display.setCursor(0, 0);
+    display.print("LoRa failed!");
     display.display();
+    tone(5, 4000, 5000);      //Pin, Freq, Duration
     while (1);
   }
-
-  display.setCursor(48, 30);
-  display.print("Radio enabled");
-  display.display();
-
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    display.setCursor(72, 30);
-    display.print("setFrequency failed!");
-    display.display();
-    while (1);
-  }
-
-  rf95.setTxPower(10, false);     //5 to 23 dBm
-
 }
 
 void loop() {
+  display.setTextSize(1);
+  BatSignal_Display();
 
-  char packRequest[6] = "Pack?";
-  rf95.send((uint8_t *)packRequest, 6);
-  rf95.waitPacketSent();
-}
-  /*uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {     // received a packet?
 
-  if (rf95.available())
-  {
-    if (rf95.recv(buf, &len))
-    {
-      buf[len] = 0;
-      display.setCursor(36, 0);
-      display.print((char*)buf);
-      display.setCursor(75, 0);
-      display.print("V");
+    display.setCursor(0, 0);
+
+    while (LoRa.available()) {
+      switch ((char)LoRa.read()) {
+        case '!':
+          smoke();
+          break;
+      }
+      display.print((char)LoRa.read());
       display.display();
-
-      tone(5, 200, 1000);
-      //Print_Voltage();
     }
   }
-}*/
+}
